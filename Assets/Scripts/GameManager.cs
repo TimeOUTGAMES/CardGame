7cb79dd14c7c7c10efc47080c1ebae04f4165543
Beytullah Cakir <betocakir52@gmail.com>
2024-11-23 22:10:27 +0300

    [Update]
    First commit and LoadingScene script has been written

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
